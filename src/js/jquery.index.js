97fd034ceb25038e79704ec018767f937e5a3765
Sergey Khmelevskoy <xs290@me.com>
2018-06-06 17:10:34 +0300

    add modal

--- src/js/jquery.index.js
+++ src/js/jquery.index.js
@@ -8,7 +8,10 @@
 
     function pageReady(){
       legacySupport();
+      initModals();
     }
+
+    pageReady();
 
     function legacySupport(){
       // svg support for IE
@@ -27,6 +30,31 @@
       e.preventDefault();
     });
 
+    function initModals(){
+      // Magnific Popup
+      var startWindowScroll = 0;
+      $('[js-popup]').magnificPopup({
+        type: 'inline',
+        fixedContentPos: true,
+        fixedBgPos: true,
+        overflowY: 'auto',
+        closeBtnInside: true,
+        preloader: false,
+        midClick: true,
+        removalDelay: 300,
+        mainClass: 'popup-buble',
+        callbacks: {
+          beforeOpen: function() {
+            startWindowScroll = _window.scrollTop();
+            // $('html').addClass('mfp-helper');
+          },
+          close: function() {
+            // $('html').removeClass('mfp-helper');
+            _window.scrollTop(startWindowScroll);
+          }
+        }
+      });
+    }
 
     $(function () {
 
